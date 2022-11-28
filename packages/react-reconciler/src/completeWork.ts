655359e7f6f6afcaa9240bc128ca0a83b2ac291a
changpengyuan <wbchangpengyuan@lattebank.com>
2022-11-28 22:37:11 +0800

    feat: 首屏渲染流程

---
+++ packages/react-reconciler/src/completeWork.ts
@@ -0,0 +1,93 @@
+import {
+	appendInitialChild,
+	Container,
+	createInstance,
+	createTextInstance,
+	Instance
+} from 'hostConfig';
+import { FiberNode } from './fiber';
+import { NoFlags } from './fiberFlags';
+import { HostComponent, HostRoot, HostText } from './workTags';
+
+// 递归中的归
+export const completeWork = (wip: FiberNode): FiberNode | null => {
+	//
+	const newProps = wip.pendingProps;
+	const current = wip.alternate;
+
+	switch (wip.tag) {
+		case HostComponent:
+			if (current !== null && wip.stateNode) {
+				// update
+			} else {
+				// 1. 创建DOM
+				const instance = createInstance(wip.type, newProps);
+				// 2. 将DOM插入到DOM树中
+				appendAllChildren(instance, wip);
+				wip.stateNode = instance;
+			}
+			bubbleProperties(wip);
+			return null;
+		case HostText:
+			if (current !== null && wip.stateNode) {
+				// update
+			} else {
+				// 1. 创建DOM
+				const instance = createTextInstance(newProps.content);
+				// 2. 将DOM插入到DOM树中
+				appendAllChildren(instance, wip);
+				wip.stateNode = instance;
+			}
+			bubbleProperties(wip);
+			return null;
+		case HostRoot:
+			bubbleProperties(wip);
+			return null;
+		default:
+			if (__DEV__) {
+				console.warn('未实现的completeWork', wip);
+			}
+			return null;
+	}
+};
+
+function appendAllChildren(parent: Container | Text, wip: FiberNode) {
+	let node = wip.child;
+
+	while (node !== null) {
+		if (node?.tag === HostComponent || node?.tag === HostText) {
+			appendInitialChild(parent as Element, node.stateNode);
+		} else if (node.child !== null) {
+			node.child.return = node;
+			node = node.child;
+			continue;
+		}
+
+		if (node === wip) {
+			return;
+		}
+
+		while (node.sibling === null) {
+			if (node.return === null || node.return === wip) {
+				return;
+			}
+			node = node?.return;
+		}
+		node.sibling.return = node.return;
+		node = node.sibling;
+	}
+}
+
+function bubbleProperties(wip: FiberNode) {
+	let subtreeFlags = NoFlags;
+	let child = wip.child;
+
+	while (child !== null) {
+		subtreeFlags |= child.subtreeFlags;
+		subtreeFlags |= child.flags;
+
+		child.return = wip;
+		child = child.sibling;
+	}
+	wip.subtreeFlags |= subtreeFlags;
+}
---
+++ packages/react-reconciler/src/workLoop.ts
@@ -0,0 +1,117 @@
+import { beginWork } from './beginWork';
+import { commitMutationEffects } from './commitWork';
+import { completeWork } from './completeWork';
+import { createWorkProgress, FiberNode, FiberRootNode } from './fiber';
+import { MutationMask, NoFlags } from './fiberFlags';
+import { HostRoot } from './workTags';
+
+let workInProgress: FiberNode | null;
+
+function prepareFreshStack(root: FiberRootNode) {
+	workInProgress = createWorkProgress(root.current, {});
+}
+
+export function scheduleUpdateOnFiber(fiber: FiberNode) {
+	// 调度功能
+	const root = markUpdateFromFiberToRoot(fiber);
+	renderRoot(root);
+}
+
+function markUpdateFromFiberToRoot(fiber: FiberNode) {
+	let node = fiber;
+	let parent = node.return;
+	while (parent !== null) {
+		node = parent;
+		parent = node.return;
+	}
+	if (node.tag === HostRoot) {
+		return node.stateNode;
+	}
+	return null;
+}
+
+function renderRoot(root: FiberRootNode) {
+	// 初始化
+	prepareFreshStack(root);
+
+	do {
+		try {
+			wookLoop();
+			break;
+		} catch (e) {
+			if (__DEV__) {
+				console.warn('workLoop 发生错误');
+			}
+			workInProgress = null;
+		}
+	} while (true);
+
+	const finishedWork = root.current.alternate;
+	root.finishedWork = finishedWork;
+
+	commitRoot(root);
+}
+
+function commitRoot(root: FiberRootNode) {
+	const finishedWork = root.finishedWork;
+
+	if (finishedWork === null) {
+		return;
+	}
+
+	if (__DEV__) {
+		console.warn('commit阶段开始', finishedWork);
+	}
+
+	// 重置
+	root.finishedWork = null;
+
+	// 判断是否存在3个子阶段需要执行的操作
+	// root flags root subtreeFlags
+	const subtreeFlags = (finishedWork.subtreeFlags & MutationMask) !== NoFlags;
+	const rootHasEffects = (finishedWork.flags & MutationMask) !== NoFlags;
+
+	if (subtreeFlags || rootHasEffects) {
+		// beforeMutation
+
+		// mutation
+		commitMutationEffects(finishedWork);
+		root.current = finishedWork; // 双缓存切换fiber树
+
+		// layout
+	} else {
+		root.current = finishedWork; // 双缓存切换fiber树
+	}
+}
+
+function wookLoop() {
+	while (workInProgress !== null) {
+		performUnitOfWork(workInProgress);
+	}
+}
+
+function performUnitOfWork(fiber: FiberNode) {
+	const next = beginWork(fiber);
+	fiber.memoizedProps = fiber.pendingProps;
+
+	if (next === null) {
+		completeUnitOfWork(fiber);
+	} else {
+		workInProgress = next;
+	}
+}
+
+function completeUnitOfWork(fiber: FiberNode) {
+	let node: FiberNode | null = fiber;
+
+	do {
+		completeWork(node);
+		const sibling = node.sibling;
+		if (sibling !== null) {
+			workInProgress = sibling;
+			return;
+		}
+		node = node.return;
+		workInProgress = node;
+	} while (node !== null);
+}
